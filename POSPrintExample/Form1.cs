a71031f056ca2760a5e378a4e12760ff5fd3e96d
faizanMayo786 <faizangame998@gmail.com>
2022-01-22 16:26:17 +0500

    My First Commit

---
+++ POSPrintExample/Form1.cs
@@ -0,0 +1,56 @@
+﻿using PrinterUtility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace POSPrintExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            PrinterUtility.EscPosEpsonCommands.EscPosEpson obj = new PrinterUtility.EscPosEpsonCommands.EscPosEpson();
+            var ByterValue = obj.Separator();
+            ByterValue = PrintExtensions.AddBytes(ByterValue, obj.CharSize.DoubleWidth6());
+            ByterValue = PrintExtensions.AddBytes(ByterValue, Encoding.ASCII.GetBytes("Invoice No. : 12345\n"));
+            ByterValue = PrintExtensions.AddBytes(ByterValue, "---------Thank You for Comming---------\n");
+
+
+
+            ByterValue = PrintExtensions.AddBytes(ByterValue, obj.Separator());
+            ByterValue = PrintExtensions.AddBytes(ByterValue, CutPage());
+
+            // PrinterUtility.PrintExtensions.Print(ByterValue, POSPrintExample.Properties.Settings.Default.PrinterName);
+
+            if (File.Exists(".\\tmpPrint.print"))
+                File.Delete(".\\tmpPrint.print");
+            File.WriteAllBytes(".\\tmpPrint.print", ByterValue);
+            RawPrinterHelper.SendFileToPrinter(POSPrintExample.Properties.Settings.Default.Printer1, ".\\tmpPrint.print");
+            try
+            {
+                File.Delete(".\\tmpPrint.print");
+            }
+            catch
+            {
+
+            }
+        }
+
+        public byte[] CutPage()
+        {
+            List<byte> oby = new List<byte>();
+            oby.Add(Convert.ToByte(Convert.ToChar(0x1D)));
+            oby.Add(Convert.ToByte('V'));
+            oby.Add((byte)66);
+            oby.Add((byte)3);
+            return oby.ToArray();
+        }
+    }
+}
---
+++ POSPrintExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace POSPrintExample
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        //[STAThread]
+        static void Main(string[] args)
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            Console.WriteLine(args);
+            string Parameter = args[0].ToLower().ToString().Replace(POSPrintExample.Properties.Settings.Default.ReplaceText.ToLower(), "");
+            string Ext = POSPrintExample.Properties.Settings.Default.PostExtn;
+
+            MessageBox.Show(Parameter);
+            string url = POSPrintExample.Properties.Settings.Default.UrlPost + "/" + Parameter + "." + Ext;
+            MessageBox.Show(url);
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            string result;
+
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                result = reader.ReadToEnd();
+            }
+            RawPrinterHelper.SendStringToPrinter(POSPrintExample.Properties.Settings.Default.PrinterName, Encoding.ASCII.GetBytes(result).ToString());
+
+
+        }
+    }
+}
